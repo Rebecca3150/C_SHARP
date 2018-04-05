e3eb1bd25c7cd43289fdede62365d0953133e7d9
Rebecca3150 <m15732157372@163.com>
2018-04-05 20:39:20 +0800

    the C#

---
+++ 净现值计算/源代码/净现值计算/参数设置/MainForm.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace 净现值计算器
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_lv = new System.Windows.Forms.TextBox();
+            this.textBox_mon = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_show = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(547, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "净现值是指特定方案未来现金流入量的现值和未来现金流出量的现值之间的差额。";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(576, 131);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "提示";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(473, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "（r是贴现率，用十进制小数值表示。t是现金流在未来出现的年数。）";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(278, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "净现值=未来报酬的总现值-初始投资现值";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "现值=第t年的值/（1+r）^t  ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBox_lv);
+            this.groupBox2.Controls.Add(this.textBox_mon);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 149);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(576, 89);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "参数设置";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(502, 44);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(15, 15);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "%";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(222, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 15);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "元";
+            // 
+            // textBox_lv
+            // 
+            this.textBox_lv.Location = new System.Drawing.Point(396, 41);
+            this.textBox_lv.Name = "textBox_lv";
+            this.textBox_lv.Size = new System.Drawing.Size(100, 25);
+            this.textBox_lv.TabIndex = 4;
+            this.textBox_lv.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_lv_KeyPress);
+            // 
+            // textBox_mon
+            // 
+            this.textBox_mon.Location = new System.Drawing.Point(116, 41);
+            this.textBox_mon.Name = "textBox_mon";
+            this.textBox_mon.Size = new System.Drawing.Size(100, 25);
+            this.textBox_mon.TabIndex = 3;
+            this.textBox_mon.TextChanged += new System.EventHandler(this.textBox_mon_TextChanged);
+            this.textBox_mon.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_mon_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(323, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "贴现率：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "项目成本：";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button_clear);
+            this.groupBox3.Controls.Add(this.button_show);
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 247);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(576, 292);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "年利润记录录入";
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(247, 240);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(150, 46);
+            this.button_clear.TabIndex = 2;
+            this.button_clear.Text = "重置结果";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // button_show
+            // 
+            this.button_show.Location = new System.Drawing.Point(403, 240);
+            this.button_show.Name = "button_show";
+            this.button_show.Size = new System.Drawing.Size(150, 46);
+            this.button_show.TabIndex = 1;
+            this.button_show.Text = "计算结果";
+            this.button_show.UseVisualStyleBackColor = true;
+            this.button_show.Click += new System.EventHandler(this.button_show_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(522, 198);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.dataGridView1_EditingControlShowing);
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle1.Format = "N2";
+            dataGridViewCellStyle1.NullValue = "0";
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column2.HeaderText = "年利润";
+            this.Column2.Name = "Column2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(427, 545);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(161, 15);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "制作者：20153150李慧";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 569);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "净现值计算器";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_lv;
+        private System.Windows.Forms.TextBox textBox_mon;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button_show;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ 净现值计算/源代码/净现值计算/参数设置/MainForm.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 净现值计算器
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public static double mon=0;
+        public static double lv=0;
+        public static int Y=0;
+     
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            SolidBrush b = new SolidBrush(this.dataGridView1.RowHeadersDefaultCellStyle.ForeColor);
+            e.Graphics.DrawString((e.RowIndex + 1).ToString(System.Globalization.CultureInfo.CurrentUICulture), this.dataGridView1.DefaultCellStyle.Font, b, e.RowBounds.Location.X + 20, e.RowBounds.Location.Y + 4);
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mon = 0;
+                Y = 0;
+                mon = 0;
+                textBox_mon.Text = "";
+                textBox_lv.Text = "";
+
+                while (dataGridView1.RowCount > 0)
+                {
+                    dataGridView1.Rows.RemoveAt(0);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("重置完毕！");
+            }
+           
+        }
+
+        private void button_show_Click(object sender, EventArgs e)
+        {
+            double M = 0;
+            double s=0;
+            double r=0;
+            int j = 0;
+            double lirun = 0;
+            lv = Convert.ToDouble(textBox_lv.Text);
+            mon = Convert.ToDouble(textBox_mon.Text);
+            Y = dataGridView1.RowCount;//总年数
+            if (Y > 0)
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count-1; i++) 
+                { 
+                  //获取每年的年利润
+                       s=Convert.ToDouble(dataGridView1.Rows[i].Cells[0].Value);
+                       //MessageBox.Show("第"+i+"年="+s);
+                       j = i + 1;
+                       r = Math.Pow((1 + lv * 0.01), j);
+                       M+= s / r;
+                       lirun += s;
+
+                }
+
+
+                double result = 0;
+                result = M - mon;
+                lirun = lirun - mon;
+                MessageBox.Show("这"+Y+"年净利润为："+lirun+",净现值为："+result);
+
+            }
+            else
+            {
+                MessageBox.Show("请输入年利润记录了！");
+            }
+        }
+
+        private void textBox_mon_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //判斷只能輸入數字和Backspace
+            if (((int)e.KeyChar < 48 || (int)e.KeyChar > 57) && (int)e.KeyChar != 8)
+                e.Handled = true;
+
+            //判断可输入小数点，但小数点不能放在数字的前面，且要保证只能输入一个小数点在正确的位置
+            if ((int)e.KeyChar == 46)                       //小数点    
+            {
+                if (textBox_mon.Text.Length <= 0)
+                    e.Handled = true;           //小数点不能在第一位    
+                else
+                {
+                    float f;
+                    float oldf;
+                    bool b1 = false, b2 = false;
+                    b1 = float.TryParse(textBox_mon.Text, out oldf);
+                    b2 = float.TryParse(textBox_mon.Text + e.KeyChar.ToString(), out f);
+                    if (b2 == false)
+                    {
+                        if (b1 == true)
+                            e.Handled = true;
+                        else
+                            e.Handled = false;
+                    }
+                    else
+                        e.Handled = false;
+                }
+            }
+        }
+
+        private void textBox_lv_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //判斷只能輸入數字和Backspace
+            if (((int)e.KeyChar < 48 || (int)e.KeyChar > 57) && (int)e.KeyChar != 8)
+                e.Handled = true;
+
+            //判断可输入小数点，但小数点不能放在数字的前面，且要保证只能输入一个小数点在正确的位置
+            if ((int)e.KeyChar == 46)                       //小数点    
+            {
+                if (textBox_lv.Text.Length <= 0)
+                    e.Handled = true;           //小数点不能在第一位    
+                else
+                {
+                    float f;
+                    float oldf;
+                    bool b1 = false, b2 = false;
+                    b1 = float.TryParse(textBox_lv.Text, out oldf);
+                    b2 = float.TryParse(textBox_lv.Text + e.KeyChar.ToString(), out f);
+                    if (b2 == false)
+                    {
+                        if (b1 == true)
+                            e.Handled = true;
+                        else
+                            e.Handled = false;
+                    }
+                    else
+                        e.Handled = false;
+                }
+            }
+        }
+       
+        //定义输入框控件
+        TextBox control;
+
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        { //只对TextBox类型的单元格进行验证
+
+            if (e.Control.GetType().BaseType.Name == "TextBox")
+            {
+
+                control = new TextBox();
+
+                control = (TextBox)e.Control;
+
+                if (control.Text == "0")    //需要限制输入数字的单元格
+                {
+
+                    control.KeyPress += new KeyPressEventHandler(control_KeyPress);
+
+                }
+
+                else
+                {
+
+                    //非数字类型单元格
+
+                    control.Leave += new EventHandler(control_Leave);
+
+                }
+
+            }
+
+        }
+
+
+        void control_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            //限制只能输入-9的数字，退格键，小数点和回车
+
+            if (((int)e.KeyChar >= 48 && (int)e.KeyChar <= 57) || e.KeyChar == 13 || e.KeyChar == 8 || e.KeyChar == 46)
+            {
+
+                e.Handled = false;
+
+            }
+
+            else
+            {
+
+                e.Handled = true;
+
+                MessageBox.Show("只能输入数字！");
+
+            }
+
+        }
+
+
+
+        void control_Leave(object sender, EventArgs e)
+        {
+
+            //如果需要限制字符串输入长度
+            /*
+            if (control.Text.Length != 11)
+            {
+
+                MessageBox.Show("只能为11位！");
+
+                control.Focus();
+
+            }
+            */
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_mon_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+     
+    }
+}
